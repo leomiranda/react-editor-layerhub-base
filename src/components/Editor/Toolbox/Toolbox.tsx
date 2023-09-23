074fdda635dc3cf2d6b4dc5531ee75f446a3e3b4
Leo Miranda <leonardomir@gmail.com>
2023-09-23 16:18:33 -0400

    refactor: svg jsx

--- src/components/Editor/Toolbox/Toolbox.tsx
+++ src/components/Editor/Toolbox/Toolbox.tsx
@@ -46,11 +46,11 @@ export default function Toolbox() {
 						xmlns="http://www.w3.org/2000/svg"
 						className="w-6 group-hover:scale-105"
 						viewBox="0 0 24 24"
-						stroke-width="1.5"
+						strokeWidth="1.5"
 						stroke="currentColor"
 						fill="none"
-						stroke-linecap="round"
-						stroke-linejoin="round"
+						strokeLinecap="round"
+						strokeLinejoin="round"
 					>
 						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 						<path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
@@ -68,11 +68,11 @@ export default function Toolbox() {
 						xmlns="http://www.w3.org/2000/svg"
 						className="w-6 group-hover:scale-105"
 						viewBox="0 0 24 24"
-						stroke-width="1.5"
+						strokeWidth="1.5"
 						stroke="currentColor"
 						fill="none"
-						stroke-linecap="round"
-						stroke-linejoin="round"
+						strokeLinecap="round"
+						strokeLinejoin="round"
 					>
 						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 						<path d="M4 8v-2a2 2 0 0 1 2 -2h2" />
@@ -91,11 +91,11 @@ export default function Toolbox() {
 						xmlns="http://www.w3.org/2000/svg"
 						className="w-6 group-hover:scale-105"
 						viewBox="0 0 24 24"
-						stroke-width="1.5"
+						strokeWidth="1.5"
 						stroke="currentColor"
 						fill="none"
-						stroke-linecap="round"
-						stroke-linejoin="round"
+						strokeLinecap="round"
+						strokeLinejoin="round"
 					>
 						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 						<path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
@@ -114,11 +114,11 @@ export default function Toolbox() {
 						xmlns="http://www.w3.org/2000/svg"
 						className="w-6 group-hover:scale-105"
 						viewBox="0 0 24 24"
-						stroke-width="1.5"
+						strokeWidth="1.5"
 						stroke="currentColor"
 						fill="none"
-						stroke-linecap="round"
-						stroke-linejoin="round"
+						strokeLinecap="round"
+						strokeLinejoin="round"
 					>
 						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 						<path d="M15 14l4 -4l-4 -4" />
@@ -134,11 +134,11 @@ export default function Toolbox() {
 						xmlns="http://www.w3.org/2000/svg"
 						className="w-6 group-hover:scale-105"
 						viewBox="0 0 24 24"
-						stroke-width="1.5"
+						strokeWidth="1.5"
 						stroke="currentColor"
 						fill="none"
-						stroke-linecap="round"
-						stroke-linejoin="round"
+						strokeLinecap="round"
+						strokeLinejoin="round"
 					>
 						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 						<path d="M9 11l-4 4l4 4m-4 -4h11a4 4 0 0 0 0 -8h-1" />
